01b375ea421c5d8c3491f4930a4bd9a451046e7c
Noah D'Alelio <dalelion@wit.edu>
2017-02-10 19:54:00 -0500

    Foundation for project added

--- NumericalIntegrationGUI/MainWindow.xaml.cs
+++ NumericalIntegrationGUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NumericalIntegrationGUI {
     /// <summary>
@@ -21,5 +10,69 @@ namespace NumericalIntegrationGUI {
         public MainWindow () {
             InitializeComponent();
         }
+
+        private void Calculate_Click (object sender, RoutedEventArgs e) {
+            StringBuilder SB = new StringBuilder();
+            SB.Clear();
+
+            double Answer, Lower = Double.Parse(LowerLimit.Text), Upper = Double.Parse(UpperLimit.Text), N = Double.Parse(NumberOfRectangles.Text);
+
+            if (RuleSelectionBox.SelectedIndex.Equals(0)) {
+                Answer = NumericalIntegration.Midpoint(Lower, Upper, N);
+                SB.AppendFormat("{0} | {1}% Error", Math.Round(Answer, 10), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 3));
+                ResultText.Text = SB.ToString();
+            } else if (RuleSelectionBox.SelectedIndex.Equals(1)) {
+                Answer = NumericalIntegration.Trapezoid(Lower, Upper, N);
+                SB.AppendFormat("{0} | {1}% Error", Math.Round(Answer, 10), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 3));
+                ResultText.Text = SB.ToString();
+            } else if (RuleSelectionBox.SelectedIndex.Equals(2)) {
+                Answer = NumericalIntegration.Simpson(Lower, Upper, N);
+                SB.AppendFormat("{0} | {1}% Error", Math.Round(Answer, 10), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 3));
+                ResultText.Text = SB.ToString();
+            } else if (RuleSelectionBox.SelectedIndex.Equals(3)){
+                ResultText.Text = NumericalIntegration.IntegralF(Lower, Upper).ToString();
+            } else {
+                ResultText.Text = "Please select a rule";
+            }
+        }
+    }
+
+    class NumericalIntegration {
+
+        private static double F (double X) {
+            return 8 * Math.Pow(X, 2) - Math.Pow(X, 5);
+        }
+
+        public static double IntegralF (double LowerBound, double UpperBound) {
+            return ((8.0 / 3.0) * Math.Pow(UpperBound, 3.0) - (Math.Pow(UpperBound, 6.0) / 6.0)) - ((8.0 / 3.0) * Math.Pow(LowerBound, 3.0) - (Math.Pow(LowerBound, 6.0) / 6.0));
+        }
+
+        public static double PercentError (double Experimental, double Actual) {
+            return ((Experimental - Actual) / Actual) * 100;
+        }
+
+        public static double Midpoint (double LowerBound, double UpperBound, double N) {
+            double DeltaX = (UpperBound - LowerBound) / N, LastX = LowerBound, Sum = 0;
+
+            for (double i = LowerBound; i <= UpperBound; i += DeltaX) {
+                Sum += F((LastX + i) / 2);
+                LastX = i;
+            }
+            return (DeltaX) * Sum;
+        }
+
+        public static double Trapezoid (double LowerBound, double UpperBound, double N) {
+            double DeltaX = (UpperBound - LowerBound) / N, Sum = 0;
+
+            for (double i = LowerBound; i <= UpperBound; i += DeltaX) {
+                Sum += 2 * F(i);
+            }
+            return (DeltaX / 2) * Sum;
+        }
+
+        public static double Simpson (double LowerBound, double UpperBound, double N) {
+            return (2 * Midpoint(LowerBound, UpperBound, N) + Trapezoid(LowerBound, UpperBound, N)) / 3;
+        }
+
     }
 }
