3ebc9454a1edb4d61dcec2b5571019adab192842
Noah D'Alelio <dalelion@wit.edu>
2017-02-15 23:22:44 -0500

    Converted code to objects.

--- NumericalIntegrationGUI/MainWindow.xaml.cs
+++ NumericalIntegrationGUI/MainWindow.xaml.cs
@@ -13,32 +13,38 @@ namespace NumericalIntegrationGUI {
 
         private void Calculate_Click (object sender, RoutedEventArgs e) {
 
+            //TODO: Make methods able to parse strings to code (System.Linq.Expressions)
+            //Can configure methods here
+            Func<Decimal, Decimal> f = (X) => 8 * Integral.Power(X, 2) - Integral.Power(X, 5);
+            Func<Decimal, Decimal, Decimal> If = (L, U) => (((Decimal)8 / 3) * Integral.Power(U, 3) - (Integral.Power(U, 6) / 6)) - (((Decimal)8 / 3) * Integral.Power(L, 3) - (Integral.Power(L, 6) / 6));
+
+            Integral integral = null;
+
             try {
 
-                Decimal Answer, Lower = Decimal.Parse(LowerLimit.Text), Upper = Decimal.Parse(UpperLimit.Text), N = Decimal.Parse(NumberOfRectangles.Text);
+                Decimal Lower = Decimal.Parse(LowerLimit.Text), Upper = Decimal.Parse(UpperLimit.Text), N = Decimal.Parse(NumberOfRectangles.Text);
 
                 if (Lower > Upper) throw new InvalidOperationException("Lower bound > Upper Bound");
 
                 switch (RuleSelectionBox.SelectedIndex) {
 
                     case 0:
-                        Answer = NumericalIntegration.Midpoint(Lower, Upper, N);
-                        ResultText.Text = String.Format("{0} | {1}% Error", Math.Round(Answer, 8), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 4));
+                        integral = new Midpoint(f, Lower, Upper, N);
                         break;
                     case 1:
-                        Answer = NumericalIntegration.Trapezoid(Lower, Upper, N);
-                        ResultText.Text = String.Format("{0} | {1}% Error", Math.Round(Answer, 8), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 4));
+                        integral = new Trapezoid(f, Lower, Upper, N);
                         break;
                     case 2:
-                        Answer = NumericalIntegration.Simpson(Lower, Upper, N);
-                        ResultText.Text = String.Format("{0} | {1}% Error", Math.Round(Answer, 8), Math.Round(NumericalIntegration.PercentError(Answer, NumericalIntegration.IntegralF(Lower, Upper)), 4));
+                        integral = new Simpson(f, Lower, Upper, N);
                         break;
                     case 3:
-                        ResultText.Text = Math.Round(NumericalIntegration.IntegralF(Lower, Upper), 24).ToString();
-                        break;
+                        ResultText.Text = Math.Round(If(Lower, Upper), 20).ToString();
+                        return;
                     default:
                         throw new Exception();
                 }
+
+                ResultText.Text = integral.ToString();
 
             } catch (Exception E) {
                 ResultText.Text = E.Message.Equals("Lower bound > Upper Bound") ? E.Message : "Error: Bad Input";
@@ -47,54 +53,112 @@ namespace NumericalIntegrationGUI {
 
     }
 
-    class NumericalIntegration {
+    public abstract class Integral {
 
-        private static Decimal Power (Decimal Base, int P) {
+        protected Decimal LowerLimit;
+        protected Decimal UpperLimit;
+        protected Decimal Number;
+        protected Decimal Result;
+        protected Decimal Actual;
+        protected Decimal Error;
+        protected Func<Decimal, Decimal> FofX;
+        //TODO implement IntegralF
+        protected Func<Decimal, Decimal> IntF;
 
-            if (P.Equals(0)) return 1;
-            if (Base.Equals(0)) return 0;
-
-            if (P % 2 == 0) {
-                return Power(Base * Base, P / 2);
-            } else {
-                return Base * Power(Base * Base, P / 2);
-            }
+        public Integral (Func<Decimal, Decimal> X, Decimal L, Decimal U, Decimal N) {
+            this.FofX = X;
+            LowerLimit = L;
+            UpperLimit = U;
+            Number = N;
+            Result = Calculate();
+            Actual = IntegralF();
+            Error = PercentError();
         }
 
-        private static Decimal F (Decimal X) {
-            return 8 * Power(X, 2) - Power(X, 5);
+        public Integral (Integral I) : this(I.FofX, I.LowerLimit, I.UpperLimit, I.Number) { }
+
+        abstract protected Decimal Calculate ();
+
+        public override string ToString () {
+            return String.Format("{0} | {1}% Error", Math.Round(Result, 8), Math.Round(Error, 4));
         }
 
-        public static Decimal IntegralF (Decimal LowerBound, Decimal UpperBound) {
-            return (((Decimal)8 / 3) * Power(UpperBound, 3) - (Power(UpperBound, 6) / 6)) - (((Decimal)8 / 3) * Power(LowerBound, 3) - (Power(LowerBound, 6) / 6));
+        public static Decimal operator * (Decimal D, Integral X) {
+            return X.Result * D;
         }
 
-        public static Decimal PercentError (Decimal Experimental, Decimal Actual) {
-            return (Math.Abs(Experimental - Actual) / Actual) * 100;
+        public static Decimal operator + (Decimal D, Integral X) {
+            return X.Result + D;
         }
 
-        public static Decimal Midpoint (Decimal LowerBound, Decimal UpperBound, Decimal N) {
-            Decimal DeltaX = (UpperBound - LowerBound) / N, LastX = LowerBound, Sum = 0;
+        public static Decimal Power (Decimal Base, int P) {
+            if (P == 0) return 1;
+            if (Base == 0) return 0;
+            return P % 2 == 0 ? Power(Base * Base, P / 2) : Base * Power(Base * Base, P / 2);
+        }
 
-            for (Decimal i = LowerBound; i <= UpperBound; i += DeltaX) {
+        protected Decimal F (Decimal X) {
+            return this.FofX(X);
+        }
+
+        public Decimal IntegralF () {
+            return (((Decimal)8 / 3) * Power(UpperLimit, 3) - (Power(UpperLimit, 6) / 6)) - (((Decimal)8 / 3) * Power(LowerLimit, 3) - (Power(LowerLimit, 6) / 6));
+        }
+
+        protected Decimal PercentError () {
+            return (Math.Abs(Result - Actual) / Actual) * 100;
+        }
+
+    }
+
+    public class Midpoint : Integral {
+
+        public Midpoint (Func<Decimal, Decimal> X, Decimal L, Decimal U, Decimal N) : base(X, L, U, N) { }
+
+        public Midpoint (Integral I) : base(I) { }
+
+        protected override Decimal Calculate () {
+            Decimal DeltaX = (UpperLimit - LowerLimit) / Number, LastX = LowerLimit, Sum = 0;
+
+            for (Decimal i = LowerLimit; i <= UpperLimit; i += DeltaX) {
                 Sum += F((LastX + i) / 2);
                 LastX = i;
             }
             return (DeltaX) * Sum;
         }
 
-        public static Decimal Trapezoid (Decimal LowerBound, Decimal UpperBound, Decimal N) {
-            Decimal DeltaX = (UpperBound - LowerBound) / N, Sum = 0;
+    }
 
-            for (Decimal i = LowerBound; i <= UpperBound; i += DeltaX) {
+    public class Trapezoid : Integral {
+
+        public Trapezoid (Func<Decimal, Decimal> X, Decimal L, Decimal U, Decimal N) : base(X, L, U, N) { }
+
+        public Trapezoid (Integral I) : base(I) { }
+
+        protected override Decimal Calculate () {
+            Decimal DeltaX = (UpperLimit - LowerLimit) / Number, Sum = 0;
+
+            for (Decimal i = LowerLimit; i <= UpperLimit; i += DeltaX) {
                 Sum += 2 * F(i);
             }
             return (DeltaX / 2) * Sum;
-        }
 
-        public static Decimal Simpson (Decimal LowerBound, Decimal UpperBound, Decimal N) {
-            return (2 * Midpoint(LowerBound, UpperBound, N) + Trapezoid(LowerBound, UpperBound, N)) / 3;
         }
 
     }
+
+    public class Simpson : Integral {
+
+        public Simpson (Func<Decimal, Decimal> X, Decimal L, Decimal U, Decimal N) : base(X, L, U, N) { }
+
+        public Simpson (Integral I) : base(I) { }
+
+        protected override Decimal Calculate () {
+
+            return (2 * new Midpoint(this) + new Trapezoid(this)) / 3;
+
+        }
+
+    }
+
 }
